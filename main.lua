edfbe5ae92ea364bf3406101e4ae8daa4c5ec0ae
Shea Mentges <Shea Mentges>
2016-12-10 22:44:46 -0800

    Added phys stuff.

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
-function 
+enemy_controller = {}
+enemy_controller.enemies = {}
 
 function love.load()
+  love.physics.setMeter(64) --the height of a meter our worlds will be 64px
+  world = love.physics.newWorld(0, 9.81*64, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
   -- success = love.window.setFullscreen(true, "desktop")
   -- Define player
   player = {}
@@ -18,9 +21,27 @@ function love.load()
     bullet.y = player.y
     table.insert(player.bullets,bullet)
   end
+  enemy_controller:spawnEnemy(30, 30)
 end
 
+function enemy_controller:spawnEnemy(x, y)
+  enemy = {}
+  enemy.x = x
+  enemy.y = y
+  enemy.width = 20
+  enemy.height = 20
+  enemy.bullets = {}
+  enemy.body = love.physics.newBody(world, 650/2, 650/2, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
+  enemy.shape = love.physics.newCircleShape(20) --the ball's shape has a radius of 20
+  fixture = love.physics.newFixture(enemy.body, enemy.shape, 1) -- Attach fixture to body and give it a density of 1.
+  fixture:setRestitution(0.9) --let the ball bounce
+  table.insert(enemy_controller.enemies,enemy)
+end
+
+
+
 function love.update(dt)
+  world:update(dt)
   unit = dt*100
   -- Handle input
   if love.keyboard.isDown('right') then
@@ -47,6 +68,10 @@ function love.draw()
   -- Draw player projectiles
   for _,v in pairs(player.bullets) do
     love.graphics.rectangle('fill',v.x,v.y,bullet.width,bullet.height)
+  end
+  -- Draw enemies
+  for _,enemy in pairs(enemy_controller.enemies) do
+    love.graphics.rectangle('fill',enemy.body:getX(),enemy.body:getY(),enemy.width,enemy.height)
   end
 end
 
