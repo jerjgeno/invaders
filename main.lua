513a52c1a40414139afea9e315718347bc256687
Shea Mentges <Shea Mentges>
2016-12-10 22:05:08 -0800

    Added stuff.

--- main.lua
+++ main.lua
@@ -1,41 +1,52 @@
-
+function 
 
 function love.load()
+  -- success = love.window.setFullscreen(true, "desktop")
+  -- Define player
   player = {}
   player.x = 100
+  player.y = 550
+  player.width = 80
+  player.height = 20
+  -- Define player projectiles
   player.bullets = {}
   player.fire = function()
     bullet = {}
-    bullet.x = player.x+40
-    bullet.y = 400
+	bullet.width = 10
+	bullet.height = 10
+    bullet.x = player.x + player.width / 2 - bullet.width / 2
+    bullet.y = player.y
     table.insert(player.bullets,bullet)
   end
 end
 
 function love.update(dt)
   unit = dt*100
+  -- Handle input
   if love.keyboard.isDown('right') then
     player.x = player.x + unit
   elseif love.keyboard.isDown('left') then
     player.x = player.x - unit
   end
-
+  if love.keyboard.isDown('escape') then
+    love.event.quit()
+  end
   if love.keyboard.isDown("space") then
     player.fire()
+  end
+  
+  -- Update other stuff
+  for _,v in pairs(player.bullets) do
+    v.y = v.y - 10
   end
 end
 
 function love.draw()
-  -- Print Love2D versoin
-  local major, minor, revision, codename = love.getVersion()
-  local str = string.format("Version %d.%d.%d - %s", major, minor, revision, codename)
-  love.graphics.print(str, 20, 20)
-
-  
-  love.graphics.rectangle('fill',player.x,400,80,20)
+  -- Draw player
+  love.graphics.rectangle('fill',player.x,player.y,player.width,player.height)
+  -- Draw player projectiles
   for _,v in pairs(player.bullets) do
-    love.graphics.rectangle('fill',v.x,v.y,10,10)
-    v.y = v.y - 10
+    love.graphics.rectangle('fill',v.x,v.y,bullet.width,bullet.height)
   end
 end
 
